4a7e32f51c02f9354eae109df375c702b3ebf5ba
Jim Kubicek <jkubicek@twitter.com>
2014-05-24 21:10:48 -0700

    Changed the on/off methods to POST

--- server.py
+++ server.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 
-from bottle import route, run, template
+from bottle import get, post, request, route, run, template
 from subprocess import call
 call(["ls", "-l"])
 import BaseHTTPServer
 
-@route('/')
+@get('/')
 def home():
     return '<h1>Welcome</h1><p>Welcome to the Raspberry Pi</p>'
     
-@route('/switch/on')
+@post('/switch/on')
 def switchOn():
     call(['wemo', 'switch', 'Living Room', 'on'])
-    return '<h1>On</h1>'
     
-@route('/switch/off')
+@post('/switch/off')
 def switchOn():
     call(['wemo', 'switch', 'Living Room', 'off'])
-    return '<h1>Off</h1>'
 
 run(host='0.0.0.0', debug=True)
